b250ac48b433abfae2493d4d777929465b4ea19b
SimpleMineCode <smc@mplugins.net>
2024-01-31 14:51:52 +0100

    Added banner

--- API/src/main/java/com/simpleminecode/api/plugin/SimplePlugin.java
+++ API/src/main/java/com/simpleminecode/api/plugin/SimplePlugin.java
@@ -1,5 +1,6 @@
 package com.simpleminecode.api.plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimplePlugin extends JavaPlugin {
@@ -25,11 +26,19 @@ public class SimplePlugin extends JavaPlugin {
         }
 
         this.starting();
+        this.printBannerToConsole();
     }
 
     @Override
     public final void onDisable() {
         this.stopping();
+    }
+
+    private void printBannerToConsole() {
+        Bukkit.getConsoleSender().sendMessage("");
+        Bukkit.getConsoleSender().sendMessage("§7This plugin uses §9SimpleLibrary §7v" + getDescription().getVersion());
+        Bukkit.getConsoleSender().sendMessage("§7Download: §9https://SimpleMineCode.com");
+        Bukkit.getConsoleSender().sendMessage("");
     }
 
     protected void loading() {}

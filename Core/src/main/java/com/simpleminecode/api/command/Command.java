626d7c6a261b2d86f7203abbdb37b1aec8a79730
SimpleMineCode <smc@mplugins.net>
2024-01-31 18:25:01 +0100

    Implement CommandExecutor to avoid custom command registration

--- Core/src/main/java/com/simpleminecode/api/command/Command.java
+++ Core/src/main/java/com/simpleminecode/api/command/Command.java
@@ -1,9 +1,16 @@
 package com.simpleminecode.api.command;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public interface Command {
+public interface Command extends CommandExecutor {
     boolean supportForConsole();
 
     void execute(CommandSender sender, String[] args);
+
+    @Override
+    default boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+        execute(sender, args);
+        return true;
+    }
 }
--- Core/src/main/java/com/simpleminecode/api/command/SimpleCommand.java
+++ Core/src/main/java/com/simpleminecode/api/command/SimpleCommand.java
@@ -1,8 +1,6 @@
 package com.simpleminecode.api.command;
 
-import org.bukkit.command.CommandSender;
-
-public abstract class SimpleCommand implements Command{
+public abstract class SimpleCommand implements Command {
     @Override
     public boolean supportForConsole() {
         return true;
